a04682c5f508940b31c01ae0b0a3e9cb69eeb73c
Khoa D. Vo <khoa.dante@gmail.com>
2022-05-21 23:09:10 +0700

    add some unit tests

---
+++ test/test_data_utils.py
@@ -0,0 +1,127 @@
+import tensorflow as tf
+from glob import glob
+
+import os
+import logging
+import unittest
+import builtins
+
+from utils.data import feed_data, PoolData
+from utils.data import feed_props_1, feed_props_2, load_function
+
+using_notebook = getattr(builtins, "__IPYTHON__", False)
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
+logging.getLogger('tensorflow').setLevel(logging.FATAL)
+
+test_batch_size = 4
+pad_img_size = 400
+final_gt_img_size = 256
+final_lq_img_size = 64
+
+kernel_size = 21
+
+test_pool_size = test_batch_size * 2
+
+class TestDataGenerator(unittest.TestCase):
+  def setUp(self):
+    data_path = os.path.abspath("./test/test_data_images/*.png")
+    test_images_paths = sorted(glob(data_path))
+
+    test_dataset = tf.data.Dataset.from_tensor_slices((test_images_paths))
+    test_dataset = test_dataset.map(load_function, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+    test_generator = test_dataset.batch(test_batch_size)
+    self.pool_test_data = PoolData(test_pool_size, test_batch_size)
+
+    iterator = iter(test_generator)
+    img, first_kernel, second_kernel, sinc_kernel = next(iterator)
+    gt_img, lq_img = feed_data(img, first_kernel, second_kernel, sinc_kernel, [feed_props_1, feed_props_2])
+
+    self.img = img
+    self.first_kernel = first_kernel
+    self.second_kernel = second_kernel
+    self.sinc_kernel = sinc_kernel
+
+    self.gt_img = gt_img
+    self.lq_img = lq_img
+
+    self.first_pool_gt_shape = (4, 256, 256, 3)
+    self.first_pool_lq_shape = (4, 64, 64, 3)
+
+    self.second_pool_gt_shape = (8, 256, 256, 3)
+    self.second_pool_lq_shape = (8, 64, 64, 3)
+
+  def test_gt_image_stats(self):
+    np_gt_img = self.gt_img.numpy()
+    t_min = np_gt_img.min()
+    t_max = np_gt_img.max()
+
+    self.assertTrue(t_min >= 0, t_min)
+    self.assertTrue(t_max <= 1, t_min)
+
+  def test_lq_image_stats(self):
+    np_lq_img = self.lq_img.numpy()
+    t_min = np_lq_img.min()
+    t_max = np_lq_img.max()
+    
+    self.assertTrue(t_min >= 0, t_min)
+    self.assertTrue(t_max <= 1, t_min)
+
+  def test_sinc_kernel_stats(self):
+    np_kernel = self.sinc_kernel.numpy()
+    t_min = np_kernel.min()
+    t_max = np_kernel.max()
+    
+    self.assertTrue(t_min >= -1, t_min)
+    self.assertTrue(t_max <= 1, t_min)
+
+  def test_first_kernel_stats(self):
+    np_kernel = self.first_kernel.numpy()
+    t_min = np_kernel.min()
+    t_max = np_kernel.max()
+    
+    self.assertTrue(t_min >= -1, t_min)
+    self.assertTrue(t_max <= 1, t_min)
+
+  def test_second_kernel_stats(self):
+    np_kernel = self.second_kernel.numpy()
+    t_min = np_kernel.min()
+    t_max = np_kernel.max()
+    
+    self.assertTrue(t_min >= -1, t_min)
+    self.assertTrue(t_max <= 1, t_min)
+
+  def test_pad_image_shape(self):
+    self.assertEqual(self.img.shape, (test_batch_size, pad_img_size, pad_img_size, 3), self.img.shape)
+
+  def test_gt_image_shape(self):
+    self.assertEqual(self.gt_img.shape, (test_batch_size, final_gt_img_size, final_gt_img_size, 3), self.gt_img.shape)
+
+  def test_lq_image_shape(self):
+    self.assertEqual(self.lq_img.shape, (test_batch_size, final_lq_img_size, final_lq_img_size, 3), self.lq_img.shape)
+
+  def test_kernel_shape(self):
+    self.assertEqual(self.sinc_kernel.shape, (test_batch_size, kernel_size, kernel_size), self.sinc_kernel.shape)
+    self.assertEqual(self.first_kernel.shape, (test_batch_size, kernel_size, kernel_size), self.first_kernel.shape)
+    self.assertEqual(self.second_kernel.shape, (test_batch_size, kernel_size, kernel_size), self.second_kernel.shape)
+
+  def test_pool_data(self):
+    gt_img, lq_img = self.pool_test_data.get_pool_data(self.gt_img, self.lq_img)
+
+    self.assertEqual(self.pool_test_data.queue_gt.shape, self.first_pool_gt_shape)
+    self.assertEqual(self.pool_test_data.queue_lr.shape, self.first_pool_lq_shape)
+
+    gt_img, lq_img = self.pool_test_data.get_pool_data(gt_img, lq_img)
+
+    self.assertEqual(self.pool_test_data.queue_gt.shape, self.second_pool_gt_shape)
+    self.assertEqual(self.pool_test_data.queue_lr.shape, self.second_pool_lq_shape)
+
+    gt_img, lq_img = self.pool_test_data.get_pool_data(gt_img, lq_img)
+
+    self.assertEqual(self.pool_test_data.queue_gt.shape, self.second_pool_gt_shape)
+    self.assertEqual(self.pool_test_data.queue_lr.shape, self.second_pool_lq_shape)
+
+if __name__ == "__main__" and not using_notebook:
+  unittest.main()
+
